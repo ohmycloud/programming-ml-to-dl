5268d1c6b6e1c267381a70139ef5c275b7febd4e
ohmycloud <ohmycloudy@gmail.com>
2024-12-21 20:41:09 +0800

    init commit

---
+++ ch02-your-first-learning-program/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ch02-your-first-learning-program!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from programming-ml-to-dl!")
+
+
+if __name__ == "__main__":
+    main()
